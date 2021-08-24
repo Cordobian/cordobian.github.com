6037e04ce863252a623195ca3ab67e61907b3ec3
Cordobian <cordobes.s.21@gmail.com>
2021-08-24 10:41:13 -0300

    Fin Proyecto 2

---
+++ Curso2P2/javascript/main.js
@@ -0,0 +1,40 @@
+console.log("Hola Mundo desde JS");
+
+//DOM
+
+// let links = document.querySelectorAll("a");
+
+// links.forEach(function(link){
+// 	console.log(link);
+// });
+
+//		Evento cerrar ventana
+
+let cerrar = document.querySelectorAll(".close");
+
+cerrar.forEach(function(cierre){
+
+	cierre.addEventListener('click',function(ev){
+		
+		//Evitamos funcionalidad predefinida
+		ev.preventDefault();
+
+		let content = document.querySelectorAll('.content');
+		content.forEach(function(contenido){
+		
+			//Quitamos vieja animacion
+			contenido.classList.remove("animate__flipInX");
+			contenido.classList.remove("animated");
+		
+			//Agregamos nueva animacion
+			contenido.classList.add("animate__flipOutX");
+			contenido.classList.add("animated");
+		
+			//Devolvemos funcionalidad de cierre
+			setTimeout(function(){
+				location.href = "/";
+			},600);
+
+		});
+	})
+});
